d1d5952c99ab1f4252dff72b08a3928e722dce01
Rasool Riffel <riffelrasool@gmail.com>
2025-08-11 08:21:37 +0300

    #games-lib: initialising the git repo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+//import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Games List App</h1>
+        <p>This is a simple application to manage your favorite games.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
