c4230ebb87138341d40c059fe70c683419cab7a2
Rasool Riffel <riffelrasool@gmail.com>
2025-08-25 19:34:28 +0300

    #games-lib: adding a form to add more data to the db (not complete)

--- frontEnd/src/components/Cards/Cards.tsx
+++ frontEnd/src/components/Cards/Cards.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Card from "./Card";
 // import ACSyndicate from "../../assets/images/AC Syndicate.avif";
 // import arkhamAsylum from "../../assets/images/arkham asylum.jpg";
@@ -37,133 +38,16 @@ const Cards = ({ games }: Games) => {
             />
           </div>
         ))}
-
-        {/* <div className="col-md-4 mb-4">
-          <Card
-            title="Assassin's Creed Syndicate"
-            imageUrl={ACSyndicate}
-            altText="Game Title 1 Image"
-          />
+      </div>
+      <div className="row">
+        <div className="col text-center">
+          <button
+            type="button"
+            className="btn btn-primary btn-lg add-game-button"
+          >
+            <Link to="/add">Add new Game</Link>
+          </button>
         </div>
-        <div className="col-md-4 mb-4">
-          <Card title="Batman: Arkham Asylum" imageUrl={arkhamAsylum} />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Batman: Arkham Knight"
-            imageUrl={arkhamKnight}
-            altText="Game Title 3 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Call of Duty: Advanced Warfare"
-            imageUrl={CODAW}
-            altText="Game Title 4 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card title="Far Cry Primal" imageUrl={farCryPrimal} />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="God of War"
-            imageUrl={godOfWar}
-            altText="Game Title 6 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Grand Theft Auto V"
-            imageUrl={GrandTheftAutoV}
-            altText="Game Title 7 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Hogwarts Legacy"
-            imageUrl={HogwartsLegacy}
-            description="Hours: 41h"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Horizon Zero Dawn"
-            imageUrl={horionZeroDawn}
-            altText="Game Title 9 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="It Takes Two"
-            imageUrl={itTakesTwo}
-            altText="Game Title 10 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card title="Mortal Kombat X" imageUrl={mortalKombatX} />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Need for Speed Heat"
-            imageUrl={NFSHeat}
-            altText="Game Title 12 Image"
-            description="Hours: 39h"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Overcooked"
-            imageUrl={overcooked}
-            altText="Game Title 13 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Return of Arkham"
-            imageUrl={returnOfArkham}
-            altText="Game Title 14 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Shadow of Mordor"
-            imageUrl={shadowOfMordor}
-            altText="Game Title 15 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Tekken 7"
-            imageUrl={tekken}
-            altText="Game Title 16 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Unravel Two"
-            imageUrl={unravelTwo}
-            altText="Game Title 17 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="Watch Dogs"
-            imageUrl={WDOG}
-            altText="Game Title 18 Image"
-          />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card title="WRC 5" imageUrl={WRC5} altText="Game Title 19 Image" />
-        </div>
-        <div className="col-md-4 mb-4">
-          <Card
-            title="WRC 7"
-            imageUrl={WRC7}
-            altText="Game Title 20 Image"
-            description="Hello World"
-          />
-        </div> */}
       </div>
     </div>
   );
--- frontEnd/src/pages/Add.tsx
+++ frontEnd/src/pages/Add.tsx
@@ -1,7 +1,46 @@
 const Add = () => {
   return (
-    <div>
-      <h1>Add</h1>
+    <div className="text-center add-page">
+      <h1>Add a new game</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="title" className="form-label">
+                  Game Title
+                </label>
+                <input type="text" className="form-control" id="title" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="imageUrl" className="form-label">
+                  Image URL
+                </label>
+                <input type="text" className="form-control" id="imageUrl" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="description" className="form-label">
+                  Description
+                </label>
+                <textarea
+                  className="form-control"
+                  id="description"
+                  rows={3}
+                ></textarea>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="cover" className="form-label">
+                  Game Cover
+                </label>
+                <input type="file" className="form-control" id="cover" />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
